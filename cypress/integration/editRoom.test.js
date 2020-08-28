036740d0447fc31ff63889b8bdcd4a0bdd943c82
James <james.kiesel@gmail.com>
2020-08-28 16:23:02 +1200

    Add edit room tests

--- cypress/integration/editRoom.test.js
+++ cypress/integration/editRoom.test.js
@@ -1,8 +1,45 @@
 describe('Edit room', () => {
-  it('Can move a ceremony on the board', () => {}) // TODO
-  it('Can copy the shareable link', () => {}) // TODO
-  it('Can collapse and expand the sidebar', () => {}) // TODO
-  it('Can add and remove a sprint week', () => {}) // TODO
+  beforeEach(() => {
+    cy.seedRoom('default').then(() => {
+      cy.setCookie('test-room-uuid', 'test-participant-uuid')
+      cy.visit('/room/test-room-uuid')
+    })
+  })
+
+  // it('Can move a ceremony on the board', () => {}) // TODO
+
+  it('Can copy the shareable link', () => {
+    cy.contains('Share room').click()
+    cy.contains('Copied to clipboard!')
+  })
+
+  it('Can collapse and expand the sidebar', () => {
+    cy.get('.sidebar-collapse').click()
+    cy.get('.sidebar.collapsed')
+    cy.get('.sidebar-collapsed-item .shareable-link')
+    cy.get('.sidebar-collapsed-item .sidebar-collapsed-participant')
+    cy.get('.sidebar-collapsed-item .sidebar-collapsed-setup-room')
+
+    cy.get('.sidebar-collapsed-item .closed').click()
+    cy.get('sidebar.expanded')
+  })
+
+  it('Can add and remove a sprint week', () => {
+    cy.contains('Add sprint week').click()
+
+    cy.wait(500)
+    cy.contains('Sprint week 2')
+    cy.fetchRoom('test-room-uuid').then(room => {
+      assert.equal(room.weekCount, 2)
+    })
+
+    cy.contains('Remove sprint week').click()
+
+    cy.wait(500)
+    cy.fetchRoom('test-room-uuid').then(room => {
+      assert.equal(room.weekCount, 1)
+    })
+  })
 
   it('Can create a new room', () => {}) // TODO (NB currently broken)
 })

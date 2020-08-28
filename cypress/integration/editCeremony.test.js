63b7c3addac740928a4c7403424b11c24a2ea7b3
James <james.kiesel@gmail.com>
2020-08-28 15:35:06 +1200

    Move create ceremony test to create ceremony file

--- cypress/integration/editCeremony.test.js
+++ cypress/integration/editCeremony.test.js
@@ -52,24 +52,4 @@ describe('Edit ceremony', () => {
       assert.equal(room.ceremonies.checkin.endTime, 75) // 60 minute meetings by default
     })
   })
-
-  it('Can make a custom ceremony', () => {
-    cy.contains('Back').click()
-    cy.contains('Add custom').click()
-    cy.get('input[name=title]').type('Test title')
-    cy.get('input[name=subheading]').type('Test subheading')
-    cy.get('textarea[name=description]').type('Test description')
-    cy.contains('Save').click()
-
-    cy.get('.ReactModal__Content .custom-card')
-    cy.contains('Back').click()
-
-    cy.fetchRoom('test-room-uuid').then(room => {
-      const ceremony = Object.values(room.ceremonies).find(({ custom }) => custom)
-      assert.equal(ceremony.title, 'Test title')
-      assert.equal(ceremony.subheading, 'Test subheading')
-      assert.equal(ceremony.description, 'Test description')
-      assert.equal(ceremony.placement, 'undecided')
-    })
-  })
 })
